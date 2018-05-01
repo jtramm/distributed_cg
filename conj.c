6550642742790c4270ba9d404724e54289859639
John Tramm <john.tramm@gmail.com>
2018-05-01 17:59:41 -0500

    on the fly matrix assembly working. serial basically done

--- conj.c
+++ conj.c
@@ -3,6 +3,33 @@
 #include <string.h>
 #include <math.h>
 
+void print_matrix(double ** A, long N )
+{
+	for( long i = 0; i < N; i++ )
+	{
+		for( long j = 0; j < N; j++ )
+		{
+			printf("%4.1lf ", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void print_vector(double * x, long N )
+{
+	long n = sqrt(N);
+	long idx = 0;
+	for( long i = 0; i < n; i++ )
+	{
+		for( long j = 0; j < n; j++ )
+		{
+			printf("%4.1lf ", x[idx]);
+			idx++;
+		}
+		printf("\n");
+	}
+}
+
 /* Allocates 2-D Contiguous Matrix */
 double ** matrix( long N )
 {
@@ -26,12 +53,54 @@ void matrix_free( double ** M)
 void matvec( double * b, double ** A, double * x, long n )
 {
 	// Wipe solution vector clean
-	memset( x, 0, n*sizeof(double));
+	memset( b, 0, n*sizeof(double));
 	
 	for (long i = 0; i < n; i++)
 		for (long j = 0; j < n; j++)
 			b[i] += (A[i][j] * x[j]);
 }
+
+// "On the fly" Matrix Vector Product for b = A * x
+// where 'A' is the known poisson operator matrix
+// that is applied without explicit storage
+void matvec_OTF( double * b, double * x, long N )
+{
+	long n = sqrt(N);
+
+	// Wipe solution vector clean
+	memset( b, 0, N*sizeof(double));
+	
+	for( long i = 0; i < N; i++ )
+	{
+		long x_i = i%n;
+
+		// Far left diagonal
+		double far_left_diag = 0.0;
+		if( i >= n )
+			far_left_diag = -1.0 * x[i-n];
+
+		// left diagonal
+		double left_diag = 0.0;
+		if( x_i != 0 )
+			left_diag = -1.0 * x[i-1];
+
+		// Main diagonal
+		double main_diag = 4.0 * x[i];
+
+		// Right diagonal
+		double right_diag = 0.0;
+		if( x_i != n-1 )
+			right_diag = -1.0 * x[i+1];
+
+		// Far right diagonal
+		double far_right_diag = 0.0;
+		if( i < N - n )
+			far_right_diag = -1.0 * x[i+n];
+
+		b[i] = far_left_diag + left_diag + main_diag + right_diag + far_right_diag;
+	}
+}
+
 
 // dot product of c = a * b
 double dotp( double * a, double * b, long n)
@@ -73,48 +142,60 @@ void axpy( double alpha, double * x, double beta, double * v, long n)
 		x[i] = alpha * x[i] + beta * v[i];
 }
 
-void conjgrad(double ** A, double * x, double * b, long n)
+void conjgrad(double ** A, double * x, double * b, long N)
 {
 	// r = -A*x + b
-	double * r = (double *) malloc( n*sizeof(double));
-	matvec(r, A, x, n);
-	axpy(-1.0, r, 1.0, b, n);
+	double * r = (double *) malloc( N*sizeof(double));
+	matvec_OTF(r, x, N);
+	axpy(-1.0, r, 1.0, b, N);
+	//printf("r = p at beginning:\n");
+	//print_vector(r, N);
 
     //p = r;
-	double * p = (double *) malloc( n*sizeof(double));
-	memcpy(p, r, n*sizeof(double));
+	double * p = (double *) malloc( N*sizeof(double));
+	memcpy(p, r, N*sizeof(double));
 
     //rsold = r' * r;
-	double rsold = dotp(r, r, n); 
+	double rsold = dotp(r, r, N); 
 
 	// Ap
-	double * Ap = (double *) malloc( n*sizeof(double));
+	double * Ap = (double *) malloc( N*sizeof(double));
 
-	for( long iter = 0; iter < n; iter++ )
+	long iter = 0;
+	for( iter = 0; iter < N; iter++ )
 	{
         //Ap = A * p;
-		matvec(Ap, A, p, n);
+		matvec_OTF(Ap, p, N);
+		//printf("Ap after iter %ld\n", iter);
+		//print_vector(Ap, N);
+		//break;
 	
         //alpha = rsold / (p' * Ap);
-		double alpha = rsold / dotp(p, Ap, n);
+		double alpha = rsold / dotp(p, Ap, N);
+		//printf("alpha = %lf after iter %ld\n", alpha, iter);
 
         //x = x + alpha * p;
-		axpy(1.0, x, alpha, p, n);
+		axpy(1.0, x, alpha, p, N);
 
         //r = r - alpha * Ap;
-		axpy(1.0, r, -alpha, Ap, n);
+		axpy(1.0, r, -alpha, Ap, N);
+		//printf("r after iter %ld\n", iter);
+		//print_vector(r, N);
 
-        double rsnew = dotp(r,r,n);
+        double rsnew = dotp(r,r,N);
 
         if( sqrt(rsnew) < 1.0e-10 )
             break;
 
         //p = (rsnew / rsold) * p + r;
-		axpy(rsnew/rsold, p, 1.0, r, n);
+		axpy(rsnew/rsold, p, 1.0, r, N);
 
         //rsold = rsnew;
 		rsold = rsnew;
+		//printf("x after iter %ld\n", iter);
+		//print_vector(x, N);
 	}
+	printf("converged in %ld iterations.\n", iter);
 
 	free(r);
 	free(p);
@@ -169,8 +250,6 @@ double get_b(long i, long j, long n)
 	double left = 0.5;
 	double right = 1.0;
 
-	long idx = i*n + j;
-
 	if( j == 0 )
 		return left;
 	else if( j == n-1 )
@@ -194,18 +273,6 @@ void fill_b(double * b, long N)
 		}
 }
 
-void print_matrix(double ** A, long N )
-{
-	for( long i = 0; i < N; i++ )
-	{
-		for( long j = 0; j < N; j++ )
-		{
-			printf("%4.1lf ", A[i][j]);
-		}
-		printf("\n");
-	}
-}
-
 int main(int argc, const char** argv){
 	
 	FILE *outfile;
@@ -227,8 +294,18 @@ int main(int argc, const char** argv){
 	// Compute elements of boundary condition vector 'b'
 	fill_b(b, N);
 
-	// Solve for 'x' solution vector
+	printf("A matrix:\n");
 	print_matrix(A, N);
+
+	printf("b vector:\n");
+	print_vector(b, N);
+	
+	printf("Running CG...\n");
+	conjgrad(A, x, b, N);
+
+	printf("Solution matrix:\n");
+	print_vector(x, N);
+
 	
 	/*
 	// print
