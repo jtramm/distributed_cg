6c101bcceb1f006849ca203d5030a89d6e398227
John Tramm <john.tramm@gmail.com>
2018-05-01 17:15:09 -0500

    proper A and b assembly done

--- conj.c
+++ conj.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
 
 /* Allocates 2-D Contiguous Matrix */
 double ** matrix( long N )
@@ -28,10 +28,9 @@ void matvec( double * b, double ** A, double * x, long n )
 	// Wipe solution vector clean
 	memset( x, 0, n*sizeof(double));
 	
-	for (long i = 0; i < n; i++){
-		for (long j = 0; j < n; j++){
+	for (long i = 0; i < n; i++)
+		for (long j = 0; j < n; j++)
 			b[i] += (A[i][j] * x[j]);
-		}
 }
 
 // dot product of c = a * b
@@ -122,75 +121,121 @@ void conjgrad(double ** A, double * x, double * b, long n)
 	free(Ap);
 }
 
+void fill_A(double ** A, long N)
+{
+	long n = sqrt(N);
+
+	for( long i = 0; i < N; i++ )
+	{
+		for( long j = 0; j < N; j++ )
+		{
+			// Compute x-coordinate of index
+			long x = j % n;
+
+			// main diagonal
+			if(i == j)
+				A[i][j] = 4.0;
+			// left diagonal
+			else if (i == j+1 )
+			{
+				A[i][j] = -1.0;
+				if(x == n-1)
+					A[i][j] = 0.0;
+			}
+			// right diagonal
+			else if (i == j-1 )
+			{
+				A[i][j] = -1.0;
+				if(x == 0)
+					A[i][j] = 0.0;
+			}
+			// far left diagonal
+			else if(j+n == i)
+				A[i][j] = -1.0;
+			// far right diagonal
+			else if(j-n == i)
+				A[i][j] = -1.0;
+			// Otherwise, A is 0
+			else
+				A[i][j] = 0.0;
+		}
+	}
+}
+
+double get_b(long i, long j, long n)
+{
+	double up = 0.0;
+	double down = 2.0;
+	double left = 0.5;
+	double right = 1.0;
+
+	long idx = i*n + j;
+
+	if( j == 0 )
+		return left;
+	else if( j == n-1 )
+		return right;
+	else if( i == 0 )
+		return up;
+	else if( i == n-1 )
+		return down;
+	
+	return 0.0;
+}
+
+void fill_b(double * b, long N)
+{
+	long n = sqrt(N);
+	for( long i = 0; i < n; i++ )
+		for( long j = 0; j < n; j++ )
+		{
+			long idx = i*n + j;
+			b[idx] = get_b(i,j,n);
+		}
+}
+
+void print_matrix(double ** A, long N )
+{
+	for( long i = 0; i < N; i++ )
+	{
+		for( long j = 0; j < N; j++ )
+		{
+			printf("%4.1lf ", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
 int main(int argc, const char** argv){
 	
 	FILE *outfile;
 		
 	// n x n square grid
-	int n = 65;
-	int nsteps = 100;
-	double alpha = 0.1;
-	double dt = 1;
-	double dx = 1;
-	double C = (alpha*dt) / (dx*dx);
-	
-	// stuff
-	double *A = (double*)calloc(n*n*n*n, sizeof(double));
-	double *x = (double*)calloc(n*n, sizeof(double));
-	double *b = (double*)calloc(n*n, sizeof(double));
-	
-	
-	// init grid
-    int x0 = n/2;
-    int y0 = n/2;
-    double a = 1.0f;
-    double sig_x = 5.0f;
-    double sig_y = 5.0f;
-	for (int j = 0; j < n; j++){
-		for (int i = 0; i < n; i++){
-			b[j * n + i] = a * exp(-1.0f * ((((i-x0)*(i-x0)) / (2*sig_x*sig_x)) + (((j-y0)*(j-y0)) / (2*sig_y*sig_y))));                                                        
-		}
-	}
-	
-	// set up A
-	for (int i = 0; i < n*n; i++){
-		A[i * n*n + i] = 1 + 4*C;
-		if (i+1 < n) A[i * n*n + i+1] = -C;
-		if (i+n < n*n) A[i * n*n + i+n] = -C;
-		if (i > 0) A[i * n*n + i-1] = -C;
-		if (i >= n) A[i * n*n + i-n] = -C;
-	}
-	
-	// advanace time
-	for (int s = 0; s < nsteps; s++){
-		printf("step = %d\n", s);
-		
-		// reset boundaries, i think this might cause issues with discontinuties
-		for (int i = 0; i < n; i++){
-			b[0 * n + i] = 0;
-			b[(n-1) * n + i] = 0;
-			b[i * n + 0] = 0;
-			b[i * n + (n-1)] = 0;
-		}
-		
-		// add source term
-		for (int j = 0; j < n; j++){
-			for (int i = 0; i < n; i++){
-				b[j * n + i] += source(i,j);
-			}
-		}
-		
-		// print
-		char buf[128];
-		sprintf(buf, "frame%d.dat", s);
-		outfile = fopen(buf, "wb");
-		fwrite(&b[0],sizeof(double),n*n,outfile);
-		fclose(outfile);
-		
-		// solve and rotate
-		solCG(n*n, A, b, x);
-		memcpy(b, x, n*n*sizeof(double));
-		
-	}
+	int n = 4;
 
+	// Dimension of operator matrix and vectors is n^2
+	int N = n*n;
+	
+	// Allocate full A matrix and vectors
+	double ** A = matrix( N );
+	double *  x = (double*) calloc(N, sizeof(double));
+	double *  b = (double*) calloc(N, sizeof(double));
+
+	// Compute elements of 'A' matrix (Poisson Operator)
+	fill_A(A, N);
+
+	// Compute elements of boundary condition vector 'b'
+	fill_b(b, N);
+
+	// Solve for 'x' solution vector
+	print_matrix(A, N);
+	
+	/*
+	// print
+	char buf[128];
+	sprintf(buf, "A.dat", s);
+	outfile = fopen(buf, "wb");
+	fwrite(&b[0],sizeof(double),n*n,outfile);
+	fclose(outfile);
+	*/
 }
